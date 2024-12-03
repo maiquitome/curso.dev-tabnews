efc2a2209bdca42944dd280c1b43e41c3bfd6b05
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-12-03 17:56:29 -0300

    feat: :sparkles: add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,36 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  // console.log("process.env.POSTGRES_DATABASE", process.env.POSTGRES_DATABASE);
+  // resultado do console.log é undefined
+
+  // Porque POSTGRES_DATABASE mesmo não existindo mais no arquivo .env não quebrou os testes?
+
+  // Quando a variável POSTGRES_DB não é fornecida para a imagem,
+  // ela usa o valor da variável POSTGRES_USER, que por padrão é postgres.
+  // Todavia, o comportamento ao qual o Filipe se refere na aula, responsável
+  // por não quebrar os testes, é atribuído ao módulo pg,
+  // que percebe que a propriedade database está com o valor undefined e
+  // que recorre então ao valor fornecido na propriedade user.
+
+  // https://curso.dev/alunos/HenriqueNas/b907f191-6823-495b-84b3-bb9494c525c4
+  // https://curso.dev/alunos/Andrei/86e5c64f-d0a7-4ecf-8803-4f406402826c
+  // https://curso.dev/alunos/Andrei/dd95f5bd-457c-4eed-921c-2cdc2de48bce
+
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    // database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,13 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   // SEM charset=utf-8
-  // response.status(200).send("alunos do curso.dev são pessoas acima da média");
+  // response
+  //  .status(200)
+  //  .send("alunos do curso.dev são pessoas acima da média");
 
   // COM charset=utf-8
   response
