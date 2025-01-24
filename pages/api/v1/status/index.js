0afca696f83a476e77350d79e579d98f533e9f2f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-24 16:33:31 -0300

    refactor: :recycle: use `next-connect` in `/migrations` and `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,82 +1,76 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller";
 
-async function status(request, response) {
-  try {
-    // const result = await database.query("SELECT 1 + 1 as sum;");
-    // console.log(result.rows);
+const router = createRouter();
 
-    const updatedAt = new Date().toISOString();
-    // retorna '2024-12-05T20:09:27.426Z' => ISO 8601
+router.get(getHandler);
 
-    // const updatedAt = Date.now();
-    // retorna 1733422612203 => Unix timestamp (milliseconds que se passaram desde 01/01/1970 00:00:00)
+export default router.handler(controller.errorHandlers);
 
-    const databaseVersionResult = await database.query("SHOW server_version;");
-    // const databaseVersionResult = await database.query("SELECT version();");
-    // const databaseVersionValue = databaseVersionResult.rows[0].version;
-    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+async function getHandler(request, response) {
+  // const result = await database.query("SELECT 1 + 1 as sum;");
+  // console.log(result.rows);
 
-    const databaseMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
-    const databaseMaxConnectionsValue =
-      databaseMaxConnectionsResult.rows[0].max_connections;
+  const updatedAt = new Date().toISOString();
+  // retorna '2024-12-05T20:09:27.426Z' => ISO 8601
 
-    // const databaseOpenedConnectionsResult = await database.query(
-    //   "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
-    // );
-    const databaseName = process.env.POSTGRES_DB;
-    // const databaseOpenedConnectionsResult = await database.query(
-    //   "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-    // );
-    // const databaseOpenedConnectionsResult = await database.query(
-    //   "SELECT count(*)::int FROM pg_stat_activity WHERE datname = '" +
-    //     databaseName +
-    //     "';",
-    // );
+  // const updatedAt = Date.now();
+  // retorna 1733422612203 => Unix timestamp (milliseconds que se passaram desde 01/01/1970 00:00:00)
 
-    // const databaseOpenedConnectionsResult = await database.query(
-    //   `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
-    // );
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  // const databaseVersionResult = await database.query("SELECT version();");
+  // const databaseVersionValue = databaseVersionResult.rows[0].version;
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-    // ! SEM SQL INJECTION
-    const databaseOpenedConnectionsResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-      values: [databaseName],
-    });
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-    // const databaseOpenedConnectionsValue =
-    //   databaseOpenedConnectionsResult.rows.length;
-    const databaseOpenedConnectionsValue =
-      databaseOpenedConnectionsResult.rows[0].count;
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
+  // );
+  const databaseName = process.env.POSTGRES_DB;
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  // );
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   "SELECT count(*)::int FROM pg_stat_activity WHERE datname = '" +
+  //     databaseName +
+  //     "';",
+  // );
 
-    // SEM charset=utf-8
-    // response
-    //  .status(200)
-    //  .send("alunos do curso.dev são pessoas acima da média");
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
+  // );
 
-    // COM charset=utf-8
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: databaseVersionValue,
-          max_connections: parseInt(databaseMaxConnectionsValue),
-          opened_connections: databaseOpenedConnectionsValue,
-        },
+  // ! SEM SQL INJECTION
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  // const databaseOpenedConnectionsValue =
+  //   databaseOpenedConnectionsResult.rows.length;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  // SEM charset=utf-8
+  // response
+  //  .status(200)
+  //  .send("alunos do curso.dev são pessoas acima da média");
+
+  // COM charset=utf-8
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
